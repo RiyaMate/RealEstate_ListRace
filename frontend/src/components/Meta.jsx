889ffe5bd498f66280173437e5ed6e3785387f88
RIYA SUBHASH MATE <145019243+RiyaMate@users.noreply.github.com>
2024-04-18 09:12:30 -0400

    first commit

---
+++ frontend/src/components/Meta.jsx
@@ -0,0 +1,19 @@
+import { Helmet } from 'react-helmet-async';
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: 'Welcome To ListRace',
+  description: 'We are the best listing site',
+  keywords: 'ListRace',
+};
+
+export default Meta;
